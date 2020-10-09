1b74d4ac4f1b9b867f7e86b2d1ceb01dabe14d4c
Rajaa Omary <omary.rajaa@gmail.com>
2020-10-09 14:27:27 +0000

    test if number is even

---
+++ even.js
@@ -0,0 +1,10 @@
+//create a function to test if number is even or not
+const isEven = function (num){
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
