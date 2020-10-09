c3d765c3e5eca22d0b741b4338ceb1d0f236d561
Rajaa Omary <omary.rajaa@gmail.com>
2020-10-09 14:20:39 +0000

    function Hello

---
+++ hello.js
@@ -0,0 +1,16 @@
+//call a function to say Hello 
+/*const sayHello = function (){
+  console.log('Hello, world');
+}
+
+sayHello();
+*/
+
+//improve the function to add the parameter name
+const sayHello = function (name){
+  console.log('Hello, ' + name);
+}
+
+sayHello('Rajaa');
+sayHello('Rayan');
+sayHello('Ferdinand');
